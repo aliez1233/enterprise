c8bffdbff0070f68596d64ebc35879de027f4b7f
Admin <https://gitee.com/pc_7_0/pc.git>
2023-06-18 23:25:22 +0800

    1

--- src/main/java/com/my/service/EmployeeService.java
+++ src/main/java/com/my/service/EmployeeService.java
@@ -1,11 +1,10 @@
-package com.my.dao;
+package com.my.service;
 
 import com.my.vo.Employee;
 
 import java.util.List;
 
-public interface EmployeeMapper {
-
+public interface EmployeeService {
     List<Employee> queryALLEmployee();// 查询全部Book,返回list集合
     int insertEmployee(Employee employee);//添加一本书
     int updateEmployee(Employee employee);//更新Book
---
+++ src/main/java/com/my/service/EmployeeServiceImpl.java
@@ -0,0 +1,32 @@
+package com.my.service;
+
+import com.my.vo.Employee;
+
+import java.util.List;
+
+public class EmployeeServiceImpl implements EmployeeService{
+    @Override
+    public List<Employee> queryALLEmployee() {
+        return null;
+    }
+
+    @Override
+    public int insertEmployee(Employee employee) {
+        return 0;
+    }
+
+    @Override
+    public int updateEmployee(Employee employee) {
+        return 0;
+    }
+
+    @Override
+    public Employee queryEmployeeById(int id) {
+        return null;
+    }
+
+    @Override
+    public int deleteEmployeeById(int id) {
+        return 0;
+    }
+}
